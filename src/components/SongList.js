4721bed5de09959f1043bbc6a77df6ffa43e4425
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-29 11:40:20 -0800

    Set up basic song list react project

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return ( 
+    <div className="song-list">
+      <ul>
+        <li>This Wild Darkness</li>
+        <li>Memory Gospel</li>
+      </ul>
+    </div>
+   );
+}
+ 
+export default SongList;
