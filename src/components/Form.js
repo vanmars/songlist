57c607ef15993f05571bf881b6df6311c481dcf2
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-29 12:18:20 -0800

    Add useEffect hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 
-const NewSongForm = () => {
+const NewSongForm = ( { addSong }) => {
+  const [title, setTitle] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+  }
+
   return ( 
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Song Name:</label>
-      <input type='text' requited />
+      <input type='text' value={title} required onChange={(e) => setTitle(e.target.value)}/>
       <input type='submit' value='Add Song' />
     </form>
    );
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
+import NewSongForm from './Form';
 
 const SongList = () => {
   const [ songs, setSongs ] = useState([
@@ -8,9 +9,19 @@ const SongList = () => {
     { title: 'this wild darkness', id: 3 }
   ]);
 
-  const addSong = () => {
-    setSongs([...songs, {title: 'new song', id: v4() }])
+  const [age, setAge] = useState(20);
+
+  const addSong = (title) => {
+    setSongs([...songs, {title, id: v4() }])
   };
+
+  useEffect(() => {
+    console.log('useEffect hook ran', songs);
+  }, [songs])
+
+  useEffect(() => {
+    console.log('useEffect hook ran', age);
+  }, [age])
 
   return ( 
     <div className="song-list">
@@ -19,7 +30,8 @@ const SongList = () => {
           return (<li key={song.id}>{song.title}</li>)
         })}
       </ul>
-      <button onClick={addSong}>Add a song</button>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
    );
 }
